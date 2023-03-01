f73725ab19b7f4a38bda0c95b5c5856960aec09d
Naman Gupta <naman312000@gmail.com>
2023-03-02 00:34:31 +0530

    projects frontend done

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,46 +1,60 @@
 import { AiOutlineProject } from 'react-icons/ai'
-import { Typography } from '@mui/material'
+import { Button,Typography } from '@mui/material'
 import React from 'react'
 import './Projects.css'
-
-
+import { Delete }from '@mui/icons-material'
+import { FaRegSmileWink } from 'react-icons/fa'
 const ProjectCard = (
-    url,
+   { url,
     projectImage,
     projectTitle,
     description,
     technologies,
-    isAdmin=false
-    ) => {
+    isAdmin=false,
+}) => {
     return(
         <>
-        <a href={url}>
-
+        <a href={url} className="projectCard" target="black">
             <div>
-                <img src={projectImage} alt="Project" />
-                <Typography variant='h6'>{projectTitle}</Typography>
+                <img src={projectImage} alt="Project"  />
+                <Typography variant='h5'>{projectTitle}</Typography>
             </div>
             <div>
                 <Typography variant='h4'>About Project</Typography>
                 <Typography>{description}</Typography>
+                <Typography variant='h6'>Tech stack : {technologies}</Typography>
             </div>
         </a>
+        {
+            isAdmin && (
+                <Button style={{color:"rgba(40,40,40,0.7)"}}><Delete /></Button>
+            )
+        }
         </>
     )
 }
 
 const Projects = () => {
-    const project = [];
+    const projects = [1,2,3];
   return (
-    <div className='Projects'>
+    <div className='projects'>
         <Typography variant='h3'>Projects <AiOutlineProject/> </Typography>
-        <div className="projectWrapper">
+        <div className="projectsWrapper">
          {
-            project.map((project,index)=>(
-                <ProjectCard />
+            projects.map((project,index)=>(
+                <ProjectCard 
+                url="https://github.com/namanguptagit"
+                projectImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVcTYUMuOkikRps_wz-URH3BdCKNfdjg0jlQ&usqp=CAU"
+                projectTitle="project1"
+                description="this is a sample project"
+                technologies="Mongodb,React,NodeJS,Express"
+                 />
             ))
          }
         </div>
+        <Typography variant='h3' style={{font : "100 1.2rem 'Ubuntu Mono'"}}>
+            All The Projects Shown Above Are Made By Me <FaRegSmileWink /> 
+        </Typography>
     </div>
   )
 }
